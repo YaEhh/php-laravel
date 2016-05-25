f69177afc093cbe80018ee69dfbf3cfb58cfa094
sselea <sselea88@gmail.com>
2016-05-25 13:01:11 -0400

    one more

--- resources/views/frontend/blog/intro.blade.php
+++ resources/views/frontend/blog/intro.blade.php
@@ -6,7 +6,7 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<title>Blog</title>
 
-  	<link href="/css/intro.css" rel="stylesheet">
+  	<link href="//css/intro.css" rel="stylesheet">
     <link rel="stylesheet" href="../public/fonts/font-awesome-4.6.3/css/font-awesome.min.css">
   </head>
   <body>
